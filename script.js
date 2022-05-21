50604b880de81316ee8712b737270afa4b09f2de
VanessaLiaw021 <vliaw021@gmail.com>
2022-05-20 21:02:40 -0400

    created a function to prompt length of password and added comments

--- script.js
+++ script.js
@@ -20,5 +20,40 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
+//Function expression to prompt user for lenght of password
+var promptLength = function() {
+
+  //Prompt user for length of password
+  userLengthInput = prompt("How long do you want your password?");
+
+  //Validate the user input
+  if (userLengthInput === "") {
+    
+    //Alert message to user to enter a number
+    alert("Please enter a number");
+
+    //Re-prompt user for length of password
+    promptLength();
+
+  } else if (userLengthInput < 8) {
+
+    //Alert message to user to enter a number greater than 8 
+    alert("Please enter a number greater than 8");
+
+    //Re-prompt user for length of password
+    promptLength();
+
+  } else if (userLengthInput > 128) {
+
+    //Alert message to user to enter a number less than 128
+    alert("Please enter a number less than 128");
+
+    //Re-prompt user for length of password
+    promptLength();
+    
+  }
+
+}
+
+//Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
