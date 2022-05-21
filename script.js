33bd15fd2214601f184f1c3c517c396eca796372
VanessaLiaw021 <vliaw021@gmail.com>
2022-05-20 20:58:16 -0400

    added global scope variables and comment on existing code

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-// Assignment Code
+//Global Variables
 var generateBtn = document.querySelector("#generate");
+var userLengthInput;
+var lowerCaseInput;
+var upperCaseInput;
+var numberCaseInput;
+var specialCaseInput;
+
 
 // Write password to the #password input
 function writePassword() {
+
+  //Call function and stored the value in a variable
   var password = generatePassword();
+
+  //Get the input text of where the password will be displayed at 
   var passwordText = document.querySelector("#password");
 
+  //Get the value of the generated password and display it
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
