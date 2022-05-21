60dd4949a223f70217c0945210e7510be681b765
VanessaLiaw021 <vliaw021@gmail.com>
2022-05-20 21:09:26 -0400

    acreated function to prompt user for lowercase, uppercase, number, and special case char and validate user input

--- script.js
+++ script.js
@@ -58,5 +58,31 @@ var promptLength = function() {
   }
 }
 
+//Function expression that confirm user choice for lower, upper, number, and special case characters
+var promptCharacters = function() {
+
+  //Confirm user for lower case letters
+  lowerCaseInput = confirm("Click Ok if you want to include lowercase letter in your password");
+
+  //Confirm user for upper case letters
+  upperCaseInput = confirm("Click OK if you want to include uppercase letter in your password");
+
+  //Confirm user for numbers
+  numberCaseInput = confirm("Click OK if you want to include numebrs in your password");
+
+  //Confirm user for special case characters
+  specialCaseInput = confirm("Click OK if you want to include special characters");
+
+  //Validate user input 
+  if (!lowerCaseInput && !upperCaseInput && !numberCaseInput && !specialCaseInput) {
+
+    //Alert user to choose at least one of the characters choice 
+    alert("You must choose at least one of the following in your password");
+
+    //Re-prompt user for choices 
+    promptCharacters();
+  }
+}
+
 //Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
